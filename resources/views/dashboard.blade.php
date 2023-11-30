ec8ada0d1eed623585fa66c025db5abf335d3ffc
Matt <azurenein@gmail.com>
2023-11-30 13:43:07 +0700

    Dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,95 @@
+<div style="width: 100%; height: 100%; position: relative; background: #FAFAFA">
+    <div style="width: 1440px; padding-left: 96px; padding-right: 96px; padding-top: 24px; padding-bottom: 24px; left: 0px; top: 0px; position: absolute; background: #FAFAFA; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); justify-content: space-between; align-items: center; display: inline-flex">
+        <div style="justify-content: flex-start; align-items: flex-start; gap: 48px; display: flex">
+            <div style="width: 24px; height: 24px; background: #D9D9D9"></div>
+            <div style="color: #198076; font-size: 20px; font-family: Poppins; font-weight: 700; line-height: 25px; word-wrap: break-word">Home</div>
+            <div style="color: #5C6670; font-size: 20px; font-family: Poppins; font-weight: 400; line-height: 25px; word-wrap: break-word">Achievement</div>
+            <div style="color: #5C6670; font-size: 20px; font-family: Poppins; font-weight: 400; line-height: 25px; word-wrap: break-word">Workshop</div>
+        </div>
+        <div style="justify-content: flex-start; align-items: flex-start; gap: 24px; display: flex">
+            <div style="padding-left: 24px; padding-right: 24px; padding-top: 8px; padding-bottom: 8px; border-radius: 8px; justify-content: center; align-items: center; gap: 10px; display: flex">
+                <div style="color: #304050; font-size: 20px; font-family: Poppins; font-weight: 500; line-height: 30px; word-wrap: break-word">Sign up</div>
+            </div>
+            <div style="padding-left: 32px; padding-right: 32px; padding-top: 8px; padding-bottom: 8px; background: #198076; border-radius: 48px; justify-content: center; align-items: center; gap: 10px; display: flex">
+                <div style="color: #FAFAFA; font-size: 20px; font-family: Poppins; font-weight: 600; line-height: 30px; word-wrap: break-word">Log in</div>
+            </div>
+        </div>
+    </div>
+    <div style="width: 599px; height: 382px; left: 96px; top: 250px; position: absolute">
+        <div style="width: 599px; left: 0px; top: 0px; position: absolute"><span style="color: #198076; font-size: 48px; font-family: Poppins; font-weight: 700; line-height: 60px; word-wrap: break-word">Elevate Workforce </span><span style="color: #D8C940; font-size: 48px; font-family: Poppins; font-weight: 700; line-height: 60px; word-wrap: break-word">Productivity</span></div>
+        <div style="width: 599px; left: 0px; top: 141px; position: absolute; color: #304050; font-size: 24px; font-family: Poppins; font-weight: 400; line-height: 30px; word-wrap: break-word">Boost your team's productivity with efficient employee data management. This platform optimizes HR processes, allowing you to elevate your workforce</div>
+        <div style="width: 212px; height: 62px; left: 82px; top: 320px; position: absolute; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+            <div style="height: 62px; padding-left: 32px; padding-right: 32px; padding-top: 16px; padding-bottom: 16px; background: #198076; border-radius: 48px; justify-content: center; align-items: center; gap: 16px; display: flex">
+                <div style="color: #EEEEEE; font-size: 20px; font-family: Poppins; font-weight: 700; line-height: 30px; word-wrap: break-word">Achievement</div>
+                <div style="width: 24px; height: 24px; position: relative; transform: rotate(180deg); transform-origin: 0 0">
+                    <div style="width: 10.51px; height: 20.01px; left: 2.50px; top: 2px; position: absolute; background: #EEEEEE; border: 1px #EEEEEE solid"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="width: 188px; height: 62px; padding-left: 32px; padding-right: 32px; padding-top: 16px; padding-bottom: 16px; left: 414px; top: 570px; position: absolute; border-radius: 48px; border: 1px #198076 solid; justify-content: center; align-items: center; gap: 16px; display: inline-flex">
+        <div style="color: #198076; font-size: 24px; font-family: Poppins; font-weight: 400; line-height: 36px; word-wrap: break-word">Sign up</div>
+        <div style="width: 24px; height: 24px; position: relative; transform: rotate(180deg); transform-origin: 0 0">
+            <div style="width: 10.51px; height: 20.01px; left: 2.50px; top: 2px; position: absolute; background: #198076"></div>
+        </div>
+    </div>
+    <div style="width: 309px; padding-left: 48px; padding-right: 48px; padding-top: 16px; padding-bottom: 16px; left: 566px; top: 1545px; position: absolute; background: #198076; border-radius: 48px; justify-content: center; align-items: center; gap: 16px; display: inline-flex">
+        <div style="color: #EEEEEE; font-size: 24px; font-family: Poppins; font-weight: 700; line-height: 36px; word-wrap: break-word">See all</div>
+    </div>
+    <div style="width: 599px; left: 421px; top: 875px; position: absolute; text-align: center; color: #198076; font-size: 48px; font-family: Poppins; font-weight: 700; line-height: 60px; word-wrap: break-word">Workshop</div>
+    <div style="width: 599px; left: 421px; top: 1756px; position: absolute; text-align: center; color: #198076; font-size: 48px; font-family: Poppins; font-weight: 700; line-height: 60px; word-wrap: break-word">Achievement</div>
+    <img style="width: 602px; height: 701px; left: 838px; top: 94px; position: absolute" src="https://via.placeholder.com/602x701" />
+    <div style="width: 400px; height: 288px; left: 1706px; top: 537.50px; position: absolute; background: #D9D9D9; border-radius: 24px"></div>
+    <div style="left: 96px; top: 1015px; position: absolute; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+        <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+            <img style="width: 400px; height: 288px; border-radius: 24px" src="https://via.placeholder.com/400x288" />
+            <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 4px; display: flex">
+                <div style="width: 400px; height: 67px; text-align: center; color: #0D1A26; font-size: 24px; font-family: Poppins; font-weight: 700; line-height: 30px; word-wrap: break-word">IoT Implementation in Business</div>
+                <div style="width: 400px; height: 67px; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 400; line-height: 25px; word-wrap: break-word">Focus on implementing Internet of Things (IoT) in the business environment.</div>
+            </div>
+        </div>
+        <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+            <img style="width: 400px; height: 288px; border-radius: 24px" src="https://via.placeholder.com/400x288" />
+            <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 4px; display: flex">
+                <div style="width: 400px; height: 67px; text-align: center; color: black; font-size: 24px; font-family: Poppins; font-weight: 700; line-height: 30px; word-wrap: break-word">Time Management </div>
+                <div style="width: 400px; height: 67px; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 400; line-height: 25px; word-wrap: break-word">Help participants manage time effectively to overcome common workplace obstacles.</div>
+            </div>
+        </div>
+        <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+            <img style="width: 400px; height: 288px; border-radius: 24px" src="https://via.placeholder.com/400x288" />
+            <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 4px; display: flex">
+                <div style="width: 400px; height: 67px; text-align: center; color: black; font-size: 24px; font-family: Poppins; font-weight: 700; line-height: 30px; word-wrap: break-word">Dynamic Presentation Skills</div>
+                <div style="width: 400px; height: 67px; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 400; line-height: 25px; word-wrap: break-word">Hone professional communication skills for impactful presentations.</div>
+            </div>
+        </div>
+    </div>
+    <div style="width: 1044px; height: 642px; left: 194px; top: 1896px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+        <img style="width: 1044px; height: 501px; border-radius: 24px" src="https://via.placeholder.com/1044x501" />
+    </div>
+    <div style="width: 1044px; height: 101px; left: 202px; top: 2437px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 4px; display: inline-flex">
+        <div style="width: 1044px; height: 110px; text-align: center; color: #0D1A26; font-size: 40px; font-family: Poppins; font-weight: 700; line-height: 30px; word-wrap: break-word">Product Design Workshop</div>
+        <div style="width: 1044px; height: 116px; text-align: center; color: black; font-size: 40px; font-family: Poppins; font-weight: 400; line-height: 25px; word-wrap: break-word">Ignite team creativity in designing innovative <br/><br/>products.</div>
+    </div>
+    <div style="width: 82px; height: 102px; left: 1344px; top: 2094px; position: absolute; transform: rotate(180deg); transform-origin: 0 0; background: white">
+        <div style="width: 35.91px; height: 85.05px; left: 8.54px; top: 8.50px; position: absolute; background: #1C1C1C; border: 1px #1C1C1C solid"></div>
+    </div>
+    <div style="width: 82px; height: 102px; left: 96px; top: 2094px; position: absolute; background: white">
+        <div style="width: 35.91px; height: 85.05px; left: 8.54px; top: 8.50px; position: absolute; background: #303030; border: 1px #303030 solid"></div>
+    </div>
+    <div style="width: 1440px; height: 459px; left: 0px; top: 2815px; position: absolute; background: #198076"></div>
+    <div style="left: 139px; top: 2912px; position: absolute; text-align: center; color: #F6F5F5; font-size: 20px; font-family: Poppins; font-weight: 600; word-wrap: break-word">Tunjungan Plaza 2 Lt.5 Surabaya, Indonesia</div>
+    <div style="left: 139px; top: 3009px; position: absolute; text-align: center; color: #F6F5F5; font-size: 20px; font-family: Poppins; font-weight: 500; word-wrap: break-word">hrdpemin@gmail.com</div>
+    <div style="left: 137px; top: 2948px; position: absolute; text-align: center; color: #F6F5F5; font-size: 20px; font-family: Poppins; font-weight: 500; word-wrap: break-word">+62 878-3954-9438<br/>+62 853-3634-9464</div>
+    <img style="width: 40px; height: 40px; left: 93px; top: 3004px; position: absolute" src="https://via.placeholder.com/40x40" />
+    <img style="width: 55px; height: 55px; left: 86px; top: 2949px; position: absolute" src="https://via.placeholder.com/55x55" />
+    <img style="width: 32px; height: 32px; left: 129px; top: 2912px; position: absolute; transform: rotate(180deg); transform-origin: 0 0" src="https://via.placeholder.com/32x32" />
+    <div style="width: 393px; height: 44px; left: 97px; top: 3138px; position: absolute">
+        <div style="width: 284px; height: 44px; left: 0px; top: 0px; position: absolute; mix-blend-mode: soft-light; background: #C4C4C4"></div>
+        <div style="width: 109px; height: 44px; left: 284px; top: 0px; position: absolute; background: #F3BB2C"></div>
+        <div style="left: 293px; top: 7px; position: absolute; text-align: center; color: black; font-size: 18px; font-family: Poppins; font-weight: 500; word-wrap: break-word">Subscribe</div>
+    </div>
+    <div style="width: 554px; left: 98px; top: 3102px; position: absolute"><span style="color: #F6F5F5; font-size: 20px; font-family: Poppins; font-style: italic; font-weight: 500; word-wrap: break-word">Subscribe</span><span style="color: #F6F5F5; font-size: 20px; font-family: Poppins; font-weight: 400; word-wrap: break-word"> to our newsletter!</span></div>
+    <div style="left: 604px; top: 3241px; position: absolute; text-align: center; color: #F9F9F9; font-size: 15px; font-family: Inter; font-weight: 400; word-wrap: break-word">Copyright 2022 C - HRD PEMIN</div>
+    <img style="width: 592px; height: 555px; left: 752px; top: 2767px; position: absolute" src="https://via.placeholder.com/592x555" />
+</div>
+
