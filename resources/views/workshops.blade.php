a12964037418823e2165c817eea1431313c8e752
hilalfauzann <107779815+hilalfauzann@users.noreply.github.com>
2023-11-30 09:04:19 +0700

    workshops

---
+++ resources/views/workshops.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style2.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="{{ URL::asset('css/detailWorkshop.css') }}">
+    <title>Detail Workshop</title>
+</head>
+<body>
+    <nav class="topnav">
+        <a href="">Home</a>
+        <a href="">Achievement</a>
+        <a href="">Workshop</a>
+    </nav>
+    
+    <div class="text-1">
+        <h1>Workshop Page</h1>
+        </div>
+    
+ <div class="text-2">
+        <h1>Explore more for unlock your skill</h1>
+         </div>
+
+    <div class="container">
+
+        <div class="card">
+            <img src="" alt="" width="600">
+            <div class="card-body">
+                <h4>Judul Workshop</h4>
+                <p>Description</p>
+            </div>
+
+            <div class="card-footer">
+                <div class="user">
+                    <div class="container1">
+                        <div class="user-info">
+                            <h5>Participants</h5>
+                            <small></small>
+                        </div>
+                            <button class="card-button">Enroll Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="" alt="" width="600">
+            <div class="card-body">
+                <h4>Judul Workshop</h4>
+                <p>Description</p>
+            </div>
+
+            <div class="card-footer">
+                <div class="user">
+                    <div class="container1">
+                        <div class="user-info">
+                            <h5>Participants</h5>
+                            <small></small>
+                        </div>
+                            <button class="card-button">Enroll Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="" alt="" width="600">
+            <div class="card-body">
+                <h4>Judul Workshop</h4>
+                <p>Description</p>
+            </div>
+
+            <div class="card-footer">
+                <div class="user">
+                    <div class="container1">
+                        <div class="user-info">
+                            <h5>Participants</h5>
+                            <small></small>
+                        </div>
+                            <button class="card-button">Enroll Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    </div>
+
+    <div class="container">
+
+        <div class="card">
+            <img src="" alt="" width="600">
+            <div class="card-body">
+                <h4>Judul Workshop</h4>
+                <p>Description</p>
+            </div>
+
+            <div class="card-footer">
+                <div class="user">
+                    <div class="container1">
+                        <div class="user-info">
+                            <h5>Participants</h5>
+                            <small></small>
+                        </div>
+                            <button class="card-button">Enroll Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="" alt="" width="600">
+            <div class="card-body">
+                <h4>Judul Workshop</h4>
+                <p>Description</p>
+            </div>
+
+            <div class="card-footer">
+                <div class="user">
+                    <div class="container1">
+                        <div class="user-info">
+                            <h5>Participants</h5>
+                            <small></small>
+                        </div>
+                            <button class="card-button">Enroll Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="" alt="" width="600">
+            <div class="card-body">
+                <h4>Judul Workshop</h4>
+                <p>Description</p>
+            </div>
+
+            <div class="card-footer">
+                <div class="user">
+                    
+                    <div class="container1">
+                    <div class="user-info">
+                        <h5>Participants</h5>
+                        <small></small>
+                    </div>
+                        <button class="card-button">Enroll Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    
+
+    </div>
+
+</body>
+</html>
